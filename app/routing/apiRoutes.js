232a86a73991c18176b7d886742ca47c2a17260b
yderek5 <yderek5@gmail.com>
2017-12-15 22:49:40 -0600

    working on it

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,8 @@ router.get('/api/friends', function(req, res) {
   res.sendFile(path.join(__dirname, '../data', 'friends.js'));
 });
 
-router.post('/api/friends', function(req, res) {
-  res.sendFile(path.join(__dirname, '../data', 'friends.js'));
+router.post("/api/friends", function(req, res) {
+  res.send(req.body);
 });
 
 module.exports = router;
