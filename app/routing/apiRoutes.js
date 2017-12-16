74107a5787f3a2ce5502a6219b79df4dd866bded
yderek5 <yderek5@gmail.com>
2017-12-15 20:56:35 -0600

    add html

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var router = express.Router();
+
+// Get friends
+router.get('/api/friends', function(req, res) {
+  res.sendFile(path.join(__dirname, '../data', 'friends.js'));
+});
+
+router.post('/api/friends', function(req, res) {
+  res.sendFile(path.join(__dirname, '../data', 'friends.js'));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var fs = require('fs');
-
 // Set up express
 // ================================================
 var app = express();
@@ -16,8 +15,8 @@ app.use(bodyParser.json());
 // Routes
 // ================================================
 var htmlRoutes = require('./app/routing/htmlRoutes.js');
-app.use(htmlRoutes);
-
+var apiRoutes = require('./app/routing/apiRoutes.js');
+app.use(htmlRoutes, apiRoutes);
 // Starts the server to begin listening
 // ================================================
 app.listen(PORT, function() {
