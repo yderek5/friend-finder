fc2e9997f71ae031537d1fcb3511d5306c17f666
yderek5 <yderek5@gmail.com>
2017-12-20 17:15:06 -0600

    finish

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var users = [
     {
-        name: 'John',
+        name: 'Derek',
         photo: "https://avatars1.githubusercontent.com/u/29961167?s=40&v=4",
         score: [
             "5",
@@ -14,7 +14,23 @@ var users = [
             "1",
             "2"
         ]
-    }
+    },
+    {
+        name: 'John',
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwi59v_O25nYAhVo34MKHVV4B1kQjRwIBw&url=https%3A%2F%2Fwww.gv.com%2Fteam%2Fjohn-zeratsky%2F&psig=AOvVaw3mkQ4Dljtael397momnKuq&ust=1513897983391933",
+        score: [
+            "2",
+            "3",
+            "5",
+            "4",
+            "3",
+            "4",
+            "2",
+            "4",
+            "2",
+            "1"
+        ]
+    },
 ];
 
 module.exports = users;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,38 @@ router.get('/api/friends', function(req, res) {
 });
 
 router.post('/api/friends', function(req, res) {
-  users.push(req.body);
+
+  // This takes the array, converts all values to integers and then adds them all up to get the total sum
+  var yourScore = req.body.score.map(function(x){return parseInt(x,10)}).reduce(add);
+  function add(a, b) {
+    return a + b;
+  }
+users.push(req.body);
+  // log the total score
+  console.log("Your score is: " + yourScore);
+
+  /*go through all users in the users array and log their total score 
+  excluding the last one (that is the user you just created)*/
+  var differenceArr = [];
+  // push them to difference array..lowest num is your match
+  for(var i=0;i<users.length-1;i++) {
+    var usersScores = users[i].score.map(function(x){return parseInt(x,10)}).reduce(add);
+    console.log("Other user score is: " + usersScores);
+    var difference = Math.abs(yourScore - usersScores);
+    differenceArr.push(difference);
+    console.log(differenceArr);
+  }
+  // find the index of the smallest number in the differenceArr
+  var indexOfSmallest = 0;
+  var value = differenceArr[0];
+  for(var k=0;k<differenceArr.length;k++) {
+    if(differenceArr[k] < value) {
+      value = differenceArr[k];
+      indexOfSmallest = k;
+    }
+  }
+  // send the match with the smallest difference
+  res.send(users[indexOfSmallest]);
 });
 
 module.exports = router;
